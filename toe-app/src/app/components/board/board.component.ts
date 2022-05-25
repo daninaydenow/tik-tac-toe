1959ff6b4d92255c45ce1df96bf4d60fb005cc40
Daniel Naydenov <daninaydenow@gmail.com>
2022-05-25 13:24:38 +0300

    create values for board component

--- toe-app/src/app/components/board/board.component.ts
+++ toe-app/src/app/components/board/board.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.css']
+  styleUrls: ['./board.component.css'],
 })
 export class BoardComponent implements OnInit {
+  squares!: any[];
+  xIsNext!: boolean;
+  winner!: string;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
