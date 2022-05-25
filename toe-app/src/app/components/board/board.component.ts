e55c0e15c5accec1c816feaf5361b07f9b3d41a0
Daniel Naydenov <daninaydenow@gmail.com>
2022-05-25 18:47:06 +0300

    fix winner prop

--- toe-app/src/app/components/board/board.component.ts
+++ toe-app/src/app/components/board/board.component.ts
@@ -28,7 +28,7 @@ export class BoardComponent implements OnInit {
   makeMove(idx: number): void {
     this.squares.splice(idx, 1, this.player);
     this.xIsNext = !this.xIsNext;
-    this.calculateWinner();
+    this.winner = this.calculateWinner();
   }
 
   calculateWinner() {
