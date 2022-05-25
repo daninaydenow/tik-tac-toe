a90a35171fad6eacf01d543f9883b3c29d707e2a
Daniel Naydenov <daninaydenow@gmail.com>
2022-05-25 13:24:07 +0300

    add value to square component

--- toe-app/src/app/components/square/square.component.ts
+++ toe-app/src/app/components/square/square.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
   templateUrl: './square.component.html',
-  styleUrls: ['./square.component.css']
+  styleUrls: ['./square.component.css'],
 })
-export class SquareComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class SquareComponent {
+  @Input() value!: 'X' | 'O';
+  constructor() {}
 }
